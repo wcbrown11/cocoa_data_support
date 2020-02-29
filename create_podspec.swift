aae8cd8abdb49daf176b66da7b55275b36fef02f
wcbrown11 <wcbrown11@gmail.com>
2020-02-29 12:06:02 -0500

    Create create_podspec.swift

---
+++ create_podspec.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let fileManager = FileManager()
+let path = fileManager.currentDirectoryPath
+print(path)
+let file FileHandle? = FileHandle(forAtWritingPath: “data_support.podspec”);
+FileHandle(forAtWritingPath: “data_support.txt”);
